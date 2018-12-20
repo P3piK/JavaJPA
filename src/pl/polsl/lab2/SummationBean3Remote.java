d1be6f88b9e72f55e438ce4bfeb8fdf2e22a3932
Piotr Gryt <grytpiotr@gmail.com>
2018-12-20 21:13:12 +0100

    Add JavaJPA to git repository

---
+++ src/pl/polsl/lab2/SummationBean3Remote.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.polsl.lab2;
+
+import javax.ejb.Remote;
+
+/**
+ *
+ * @author P3p!K-MSI
+ */
+@Remote
+public interface SummationBean3Remote {
+
+    int add(int a, int b);
+}
